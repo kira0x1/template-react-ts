20a8000bf52cc664bfe5243049793384e38e7d16
Kira <kira.eva@protonmail.com>
2024-10-04 12:11:52 +0100

    delete package-lock

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -6,6 +6,14 @@ export class Game extends Scene {
     background: Phaser.GameObjects.Image;
     gameText: Phaser.GameObjects.Text;
     player: Phaser.GameObjects.Sprite;
+    keys: {
+        up: string;
+        right: string;
+        left: string;
+        down: string;
+    };
+
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
     isRight = true;
 
     constructor() {
@@ -17,18 +25,28 @@ export class Game extends Scene {
         this.camera.setBackgroundColor(0x1199aa);
         EventBus.emit("current-scene-ready", this);
         this.player = this.add.sprite(150, 150, "star");
+
+        this.cursors = this.input
+            .keyboard!.addCapture("W,S,A,D")
+            .createCursorKeys();
     }
 
     update() {
         const speed = 4;
         const vel = this.isRight ? speed : -speed;
-        this.player.x += vel * this.time.timeScale;
+        // this.player.x += vel * this.time.timeScale;
 
-        if (this.player.x >= 1024 && this.isRight) {
-            this.isRight = false;
-        } else if (this.player.x <= 0 && !this.isRight) {
-            this.isRight = true;
+        if (this.cursors.right.isDown) {
+            this.player.x += vel * this.time.timeScale;
+        } else if (this.cursors.left.isDown) {
+            this.player.x -= vel * this.time.timeScale;
         }
+
+        // if (this.player.x >= 1024 && this.isRight) {
+        //     this.isRight = false;
+        // } else if (this.player.x <= 0 && !this.isRight) {
+        //     this.isRight = true;
+        // }
     }
 
     changeScene() {
