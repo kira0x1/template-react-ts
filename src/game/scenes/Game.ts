0bf7b714c044a9500c77b748771879e03f79dd2b
Kira <kira.eva@protonmail.com>
2024-10-04 11:52:40 +0100

    convert to bun

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,36 +1,37 @@
-import { EventBus } from '../EventBus';
-import { Scene } from 'phaser';
+import { EventBus } from "../EventBus";
+import { Scene } from "phaser";
 
-export class Game extends Scene
-{
+export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     gameText: Phaser.GameObjects.Text;
+    player: Phaser.GameObjects.Sprite;
+    isRight = true;
 
-    constructor ()
-    {
-        super('Game');
+    constructor() {
+        super("Game");
     }
 
-    create ()
-    {
+    create() {
         this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
-
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
-
-        this.gameText = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
-
-        EventBus.emit('current-scene-ready', this);
+        this.camera.setBackgroundColor(0x1199aa);
+        EventBus.emit("current-scene-ready", this);
+        this.player = this.add.sprite(150, 150, "star");
     }
 
-    changeScene ()
-    {
-        this.scene.start('GameOver');
+    update() {
+        const speed = 4;
+        const vel = this.isRight ? speed : -speed;
+        this.player.x += vel * this.time.timeScale;
+
+        if (this.player.x >= 1024 && this.isRight) {
+            this.isRight = false;
+        } else if (this.player.x <= 0 && !this.isRight) {
+            this.isRight = true;
+        }
+    }
+
+    changeScene() {
+        this.scene.start("GameOver");
     }
 }
